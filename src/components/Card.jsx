bbbfd2f37fa3ebaeec72bc0ba88bdbf1ea2ab837
AYZBTR <bhattaraiaayush.78@gmail.com>
2024-12-25 03:32:28 +0200

    applying js

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { MdOutlineFileDownload } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
 
 
-function Card() {
+function Card({data}) {
   return (
     <div className="relative w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
       <FaRegFileAlt />
       <p className="text-sm leading-tight mt-5 font-semibold">
-        Lorem ipsum dolor sit amet, consectetur adipisicing.
+        {data.desc}
       </p>
       <div className="footer absolute bottom-0 w-full left-0">
         <div className="flex items-center justify-between px-8 py-3 mb-3 ">
-          <h5>.4mb</h5>
+          <h5>{data.filesize}</h5>
           <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center"> 
-            <MdOutlineFileDownload size=".7em" color="#fff" />
+            {data.close ? <IoClose /> : <MdOutlineFileDownload size=".7em" color="#fff" />}
+            
             </span>
         </div>
-        <div className="tag w-full py-4 bg-green-600 flex items-center justify-center"> 
-          <h3 className="text-sm font-semibold">Download Now</h3>
+        {
+          data.tag.isOpen && (
+            <div className="tag w-full py-4 bg-green-600 flex items-center justify-center"> 
+            <h3 className="text-sm font-semibold">Download Now</h3>
         </div>
+          )}
+        
+        
       </div>
     </div>
   );
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import Card from './Card'
+import React, { useState } from "react";
+import Card from "./Card";
 
 function Foreground() {
+  const data = [
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+  ];
+  useState();
   return (
-    <div className='fixed top-0 left-0 z-[3] w-full h-full'> 
-     <Card/>
-     
-        
+    <div className="fixed top-0 left-0 z-[3] w-full h-full">
+      {data.map((item, index)=>(
+        <Card data={item}/>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Foreground
+export default Foreground;
