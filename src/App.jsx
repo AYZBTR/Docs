db4d865493782db177d149084b6e8b9df8d00cd7
AYZBTR <bhattaraiaayush.78@gmail.com>
2024-12-25 02:09:16 +0200

    Docs

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='relative w-full h-screen bg-zinc-800'>
+      <div className='w-full py-10 flex justify-center text-zinc-200 '> Documents.</div>
+      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tight font-semibold text-zinc-900'>Docs.</h1>
+    </div>
+  )
+}
+
+export default App
